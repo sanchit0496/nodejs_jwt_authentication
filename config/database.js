5f6c732d41a17a5d255d54ed381e2c233a21c36f
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-04-07 16:42:21 +0530

    have removed extra code not required for this POC

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const RDS_PORT = process.env.RDS_PORT 
-const RDS_HOSTNAME = process.env.RDS_HOSTNAME 
-const RDS_PASSWORD = process.env.RDS_PASSWORD || ""
-const RDS_USERNAME = process.env.RDS_USERNAME
-const RDS_DB_NAME = process.env.RDS_DB_NAME; // Make sure to add this to your environment variables
+const DB_HOST = process.env.DB_HOST || "localhost"; // Default to localhost if not specified
+const DB_PORT = process.env.DB_PORT || 3306; // Default MySQL port is 3306
+const DB_USERNAME = "root"; // Ensure this is set in your .env file
+const DB_PASSWORD = process.env.DB_PASSWORD || ""; // Default to empty if not specified
+const DB_NAME = "application"; // Ensure this is set in your .env file
 
-const sequelize = new Sequelize(RDS_DB_NAME, RDS_USERNAME, RDS_PASSWORD, {
-    host: RDS_HOSTNAME,
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
     dialect: 'mysql', // or another dialect if you're not using MySQL
 });
+
 
 module.exports = sequelize;
